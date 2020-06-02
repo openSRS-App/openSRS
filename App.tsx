c5dfa7de9a10a72d987777783ea5586e5a73f3e7
Alexander Infante <alexinfante17@gmail.com>
2020-06-02 10:52:33 -0700

    Testing application for design layout with React Native.

--- App.tsx
+++ App.tsx
@@ -4,12 +4,12 @@ import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>YO THIS SHIT BUILDS</Text>
+    <Text category='h1'>Cats are the best</Text>
   </Layout>
 );
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
+  <ApplicationProvider {...eva} theme={eva.dark}>
     <HomeScreen />
   </ApplicationProvider>
 );
