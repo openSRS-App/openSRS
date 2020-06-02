71f1ded395468beb8293ad12824b976d1fb5edd4
jayacados <jayacados@gmail.com>
2020-06-02 15:13:25 -0700

    (feat) setup Cards_frame structure and file

---
+++ components/Cards_frame/Cards_frame.tsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import { View } from 'react-native'
+// import { Layout, Text, Button, CheckBox } from '@ui-kitten/components';
+
+
+// export default function Deck_frame(): React.ReactElement {
+//   return (
+//     <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+//       <Button style={{ alignSelf: "flex-start", margin: '1em' }}>Menu</Button>
+
+//       <Text category='h1'>Decks:</Text>
+//       <View style={{ flexDirection: 'row' }}>
+//         <Button style={{ margin: '1em' }}>Math</Button>
+//         <CheckBox />
+//       </ View>
+//       <View style={{ flexDirection: 'row' }}>
+//         <Button style={{ margin: '1em' }}>Science</Button>
+//         <CheckBox />
+//       </ View>
+//       <View style={{ flexDirection: 'row' }}>
+//         <Button style={{ margin: '1em' }}>Development</Button>
+//         <CheckBox />
+//       </ View>
+//     </Layout>
+//   )
+// }
