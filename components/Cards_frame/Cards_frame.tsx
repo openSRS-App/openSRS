ab3e62faaf5556fc67e760331d170e368e8976fc
jayacados <jayacados@gmail.com>
2020-06-02 15:38:57 -0700

    (feat) complete cards component

--- components/Cards_frame/Cards_frame.tsx
+++ components/Cards_frame/Cards_frame.tsx
@@ -1,26 +1,22 @@
-// import React from 'react'
-// import { View } from 'react-native'
-// import { Layout, Text, Button, CheckBox } from '@ui-kitten/components';
+import React from 'react'
+import { View } from 'react-native'
+import { Layout, Text, Button, CheckBox } from '@ui-kitten/components';
 
 
-// export default function Deck_frame(): React.ReactElement {
-//   return (
-//     <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
-//       <Button style={{ alignSelf: "flex-start", margin: '1em' }}>Menu</Button>
+export default function Cards_frame(): React.ReactElement {
+  return (
+    <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
 
-//       <Text category='h1'>Decks:</Text>
-//       <View style={{ flexDirection: 'row' }}>
-//         <Button style={{ margin: '1em' }}>Math</Button>
-//         <CheckBox />
-//       </ View>
-//       <View style={{ flexDirection: 'row' }}>
-//         <Button style={{ margin: '1em' }}>Science</Button>
-//         <CheckBox />
-//       </ View>
-//       <View style={{ flexDirection: 'row' }}>
-//         <Button style={{ margin: '1em' }}>Development</Button>
-//         <CheckBox />
-//       </ View>
-//     </Layout>
-//   )
-// }
+      <View style={{ flexDirection: 'row' }}>
+        <Button style={{ margin: '1em' }}>Add a Card</Button>
+      </ View>
+      <View style={{ flexDirection: 'row' }}>
+        <Button style={{ margin: '1em' }}>Edit a Card</Button>
+      </ View>
+      <View style={{ flexDirection: 'row' }}>
+        <Button style={{ margin: '1em' }}>Delete a Card</Button>
+      </ View>
+
+    </Layout>
+  )
+}
