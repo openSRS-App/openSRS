e6843df89f83d311a6f8a4501454d4262968727e
cmzhong <cheriemzhong@gmail.com>
2020-06-02 14:10:52 -0700

    Fixing component routing in App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider} from '@ui-kitten/components';
-// import Home_frame from './components/Home_frame/Home_frame'
-import Login_frame from './components/Login_frame/Login_frame'
+import Home_frame from './components/Home_frame/Home_frame'
+
 
 
 export default () => (
   <ApplicationProvider {...eva} theme={eva.light}>
-    {/* <Home_frame /> */}
-    <Login_frame />
+    <Home_frame />
   </ApplicationProvider>
 );
