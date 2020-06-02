c2c7e82010266f0f48f34515ebce4d793ad47f3d
Christian Hubbard <chrishubbard92@hotmail.com>
2020-06-02 16:58:54 -0700

    Adding Server

--- components/Home_frame/Home_frame.tsx
+++ components/Home_frame/Home_frame.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { Layout, Text, Button, Menu, MenuGroup, MenuItem, Card } from '@ui-kitten/components';
+import { View } from 'react-native'
+import { Layout, Text, Button, Menu, MenuGroup, MenuItem, Card,  } from '@ui-kitten/components';
 
 export default function Home_frame() {
     return (
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <view style={{alignSelf:"flex-start", padding:"10px"}}>
+        <Layout style={{display:"flex", justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{alignSelf:"flex-start"}}>
                 <Button >Menu</Button>
-            </view>
-            <view style={{flex: 1, alignSelf:'center', paddingTop:'300px', paddingBottom:'300px'}}>
-                <Card style={{alignSelf:"center", paddingTop: '80px', paddingBottom:'80px'}}>
+            </View>
+            <View style={{display:"flex", alignSelf:'center'}}>
+                <Card style={{alignSelf:"center"}}>
                     <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
                     <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
                 </Card>
-            </view>
-            <view style={{padding:'120px'}}>
+            </View>
+            <View >
                 <Button>Study!</Button>
-            </view>
+            </View>
         </Layout>
     )
 }
