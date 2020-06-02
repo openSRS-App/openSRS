8f855e57077db7a8629bd55eb885f0e1859ee259
Christian Hubbard <chrishubbard92@hotmail.com>
2020-06-02 11:25:17 -0700

    Adding home_frame spacing

--- components/Home_frame/Home_frame.tsx
+++ components/Home_frame/Home_frame.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Layout, Text, Button, Menu, MenuGroup, MenuItem } from '@ui-kitten/components';
+import { Layout, Text, Button, Menu, MenuGroup, MenuItem, Card } from '@ui-kitten/components';
 
 
 export default function Home_frame() {
     return (
         <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Button style={{alignSelf:"flex-start"}}>Menu</Button>
-            
-            <Text category='h1'>This is the home frame</Text>
-            <Button>button, yo</Button>
+            <view style={{alignSelf:"flex-start", padding:"10px"}}>
+                <Button >Menu</Button>
+            </view>
+            <view style={{flex: 1, alignSelf:'center', paddingTop:'300px', paddingBottom:'300px'}}>
+                <Card style={{alignSelf:"center", paddingTop: '80px', paddingBottom:'80px'}}>
+                    <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
+                    <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
+                </Card>
+            </view>
+            <view style={{padding:'120px'}}>
+                <Button>Study!</Button>
+            </view>
         </Layout>
     )
 }
