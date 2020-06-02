0a868c0be2b78df3fb92ef7c373d41a5f6fad66f
Alexander Infante <alexinfante17@gmail.com>
2020-06-02 16:18:38 -0700

    Adjusting the Home_frame to ensure that it builds properly for iOS clients. Adjusted the padding styling to clear through incorrect rendering.

--- components/Home_frame/Home_frame.tsx
+++ components/Home_frame/Home_frame.tsx
@@ -8,16 +8,16 @@ import { Layout, Text, Button, Menu, MenuGroup, MenuItem, Card } from '@ui-kitte
 //             <View style={{alignSelf:"flex-start", padding:"10px"}}>
 //                 <Button >Menu</Button>
 //             </View>
-//             {/* <view style={{flex: 1, alignSelf:'center', paddingTop:'300px', paddingBottom:'300px'}}>
+//             <view style={{flex: 1, alignSelf:'center', paddingTop:'300px', paddingBottom:'300px'}}>
 //                 <Card >
 //                 // style={{alignSelf:"center", paddingTop: '80px', paddingBottom:'80px'}}>
 //                     <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
 //                     <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
 //                 </Card>
-//             </view> */}
-//             {/* <view style={{padding:'120px'}}>
+//             </view>
+//             <view style={{padding:'120px'}}>
 //                 <Button>Study!</Button>
-//             </view> */}
+//             </view>
 //         </Layout>
 //     )
 // }
@@ -26,19 +26,21 @@ export default function Home_frame() {
         return (
             <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
                 <View 
-                style={{alignSelf:"flex-start", padding:"10%"}}
+                style={{alignSelf:"flex-start", padding: 20}}
                 >
                     <Button >Menu</Button>
                 </View>
-                <View style={{flex: 1, alignSelf:'center'}}>
+                <View 
+                style={{flex: 1, alignSelf:'center', paddingTop: 100, paddingBottom: 100}}
+                >
                     <Card
-                    style={{alignSelf:"center"}}
+                    style={{alignSelf:"center", paddingTop: 60, paddingBottom: 50}}
                     >
                         <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
                         <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
                     </Card>
                 </View> 
-                <View>
+                <View style={{paddingBottom: 90}}>
                     <Button>Study!</Button>
                 </View>
             </Layout>
