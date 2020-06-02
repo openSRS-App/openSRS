5945f3285f9427b21f5eeed8943d76b461fe6d83
Alexander Infante <alexinfante17@gmail.com>
2020-06-02 15:53:52 -0700

    Resolving the padding issue with React Native. Committing for a working node that displays properly. Pending adjustment for stylistic purposes.

--- components/Home_frame/Home_frame.tsx
+++ components/Home_frame/Home_frame.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { View } from 'react-native';
 import { Layout, Text, Button, Menu, MenuGroup, MenuItem, Card } from '@ui-kitten/components';
 
+// export default function Home_frame() {
+//     return (
+//         <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//             <View style={{alignSelf:"flex-start", padding:"10px"}}>
+//                 <Button >Menu</Button>
+//             </View>
+//             {/* <view style={{flex: 1, alignSelf:'center', paddingTop:'300px', paddingBottom:'300px'}}>
+//                 <Card >
+//                 // style={{alignSelf:"center", paddingTop: '80px', paddingBottom:'80px'}}>
+//                     <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
+//                     <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
+//                 </Card>
+//             </view> */}
+//             {/* <view style={{padding:'120px'}}>
+//                 <Button>Study!</Button>
+//             </view> */}
+//         </Layout>
+//     )
+// }
+
 export default function Home_frame() {
-    return (
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <view style={{alignSelf:"flex-start", padding:"10px"}}>
-                <Button >Menu</Button>
-            </view>
-            <view style={{flex: 1, alignSelf:'center', paddingTop:'300px', paddingBottom:'300px'}}>
-                <Card style={{alignSelf:"center", paddingTop: '80px', paddingBottom:'80px'}}>
-                    <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
-                    <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
-                </Card>
-            </view>
-            <view style={{padding:'120px'}}>
-                <Button>Study!</Button>
-            </view>
-        </Layout>
-    )
-}
+        return (
+            <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <View 
+                style={{alignSelf:"flex-start", padding:"10%"}}
+                >
+                    <Button >Menu</Button>
+                </View>
+                <View style={{flex: 1, alignSelf:'center'}}>
+                    <Card
+                    style={{alignSelf:"center"}}
+                    >
+                        <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
+                        <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
+                    </Card>
+                </View> 
+                <View>
+                    <Button>Study!</Button>
+                </View>
+            </Layout>
+        )
+    }
