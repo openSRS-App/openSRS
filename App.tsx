9fb6b497a6e03af5bcc22a7a3003e195b57f4993
jayacados <jayacados@gmail.com>
2020-06-02 21:01:27 -0700

    (refactor) remove comments from several component files
    modify App.tsx, Home_frame.tsx, Menu_frame.tsx to remove previous code comments

--- App.tsx
+++ App.tsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import * as eva from '@eva-design/eva';
-// import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-// import { EvaIconsPack } from '@ui-kitten/eva-icons';
-// import Home_frame from './components/Home_frame/Home_frame';
-// import FlashCard_frame from './components/FlashCard_frame/FlashCard_frame';
-
-// export default () => (
-//   <>
-//   <IconRegistry icons={EvaIconsPack} />
-//     <ApplicationProvider {...eva} theme={eva.light}>
-//       <Home_frame />
-//       {/* <FlashCard_frame /> */}
-//     </ApplicationProvider>
-//   </>
-// );
-
-// ================================================
-// ================================================
-// ======== React Navigation Begins Below =========
-// ================================================
-// ================================================
 
 // Import React Native stuff
 import * as React from 'react';
--- components/Home_frame/Home_frame.tsx
+++ components/Home_frame/Home_frame.tsx
@@ -2,26 +2,6 @@ import React from 'react';
 import { View } from 'react-native';
 import { Layout, Text, Button, Menu, MenuGroup, MenuItem, Card } from '@ui-kitten/components';
 
-// export default function Home_frame() {
-//     return (
-//         <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-//             <View style={{alignSelf:"flex-start", padding:"10px"}}>
-//                 <Button >Menu</Button>
-//             </View>
-//             <view style={{flex: 1, alignSelf:'center', paddingTop:'300px', paddingBottom:'300px'}}>
-//                 <Card >
-//                 // style={{alignSelf:"center", paddingTop: '80px', paddingBottom:'80px'}}>
-//                     <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
-//                     <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
-//                 </Card>
-//             </view>
-//             <view style={{padding:'120px'}}>
-//                 <Button>Study!</Button>
-//             </view>
-//         </Layout>
-//     )
-// }
-
 interface ButtonTitle {
     title: string;
 }
