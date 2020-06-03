10f2b9d965ad72709aa8518fe9d5bd5fcbf7346c
jayacados <jayacados@gmail.com>
2020-06-02 20:56:10 -0700

    (feat) add react navigation to home and menu component
    setup react navigation in App.tsx, Home_frame.tsx, Menu_frame.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,69 @@
-import React from 'react';
+// import React from 'react';
+// import * as eva from '@eva-design/eva';
+// import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+// import { EvaIconsPack } from '@ui-kitten/eva-icons';
+// import Home_frame from './components/Home_frame/Home_frame';
+// import FlashCard_frame from './components/FlashCard_frame/FlashCard_frame';
+
+// export default () => (
+//   <>
+//   <IconRegistry icons={EvaIconsPack} />
+//     <ApplicationProvider {...eva} theme={eva.light}>
+//       <Home_frame />
+//       {/* <FlashCard_frame /> */}
+//     </ApplicationProvider>
+//   </>
+// );
+
+// ================================================
+// ================================================
+// ======== React Navigation Begins Below =========
+// ================================================
+// ================================================
+
+// Import React Native stuff
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Importing Components
+import Home_frame from './components/Home_frame/Home_frame';
+import Cards_frame from './components/Cards_frame/Cards_frame';
+import Deck_frame from './components/Deck_frame/Deck_frame';
+import FlashCard_frame from './components/FlashCard_frame/FlashCard_frame'
+import Login_frame from './components/Login_frame/Login_frame';
+import Menu_frame from './components/Menu_frame/Menu_frame'
+import Account_frame from './components/Account_frame/Account_frame'
+// Importing Eva stuff
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import Home_frame from './components/Home_frame/Home_frame';
-import FlashCard_frame from './components/FlashCard_frame/FlashCard_frame';
 
+const Stack = createStackNavigator();
 
+function App() {
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          <Stack.Navigator>
 
+            <Stack.Screen name="Home" component={Home_frame} />
+            <Stack.Screen name="Card" component={Cards_frame} />
+            <Stack.Screen name="Deck" component={Deck_frame} />
+            <Stack.Screen name="FlashCard" component={FlashCard_frame} />
+            <Stack.Screen name="Login" component={Login_frame} />
+            <Stack.Screen name="Menu" component={Menu_frame} />
+            <Stack.Screen name="Account" component={Account_frame} />
 
-export default () => (
-  <>
-  <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Home_frame />
-      {/* <FlashCard_frame /> */}
-    </ApplicationProvider>
-  </>
-);
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
+  );
+}
+
+export default App;
+
--- components/Home_frame/Home_frame.tsx
+++ components/Home_frame/Home_frame.tsx
@@ -22,27 +22,37 @@ import { Layout, Text, Button, Menu, MenuGroup, MenuItem, Card } from '@ui-kitte
 //     )
 // }
 
-export default function Home_frame() {
-        return (
-            <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <View 
-                style={{alignSelf:"flex-start", padding: 20}}
+interface ButtonTitle {
+    title: string;
+}
+
+export default function Home_frame({ navigation }: any) {
+    return (
+        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View
+                style={{ alignSelf: "flex-start", padding: 20 }}
+            >
+                <Button
+                    title="This leads to the Menu Component"
+                    onPress={() => navigation.navigate('Menu')}
+                >Menu</Button>
+            </View>
+            <View
+                style={{ flex: 1, alignSelf: 'center', paddingTop: 100, paddingBottom: 100 }}
+            >
+                <Card
+                    style={{ alignSelf: "center", paddingTop: 60, paddingBottom: 50 }}
                 >
-                    <Button >Menu</Button>
-                </View>
-                <View 
-                style={{flex: 1, alignSelf:'center', paddingTop: 100, paddingBottom: 100}}
-                >
-                    <Card
-                    style={{alignSelf:"center", paddingTop: 60, paddingBottom: 50}}
-                    >
-                        <Text category='h1' style={{alignSelf:"center"}}>Welcome to OpenSRS</Text>
-                        <Text category='h5' style={{alignSelf:"center"}}>The Open Source SRS</Text>
-                    </Card>
-                </View> 
-                <View style={{paddingBottom: 90}}>
-                    <Button>Study!</Button>
-                </View>
-            </Layout>
-        )
-    }
+                    <Text category='h1' style={{ alignSelf: "center" }}>Welcome to OpenSRS</Text>
+                    <Text category='h5' style={{ alignSelf: "center" }}>The Open Source SRS</Text>
+                </Card>
+            </View>
+            <View style={{ paddingBottom: 90 }}>
+                <Button
+                    title="Flash Card Component"
+                    onPress={() => navigation.navigate('FlashCard')}
+                >Study!</Button>
+            </View>
+        </Layout >
+    )
+}
