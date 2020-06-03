b15986a11908f5b9fe81e425d83fea92ce7584d0
Christian Hubbard <chrishubbard92@hotmail.com>
2020-06-03 11:15:30 -0700

    Adding Mongoose Model

--- server/index.js
+++ server/index.js
@@ -1,50 +1,8 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const typeDefs = require('./typedefs');
+const resolvers = require('./resolvers');
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Users {
-    username: String,
-    id: ID,
-    decks: String,
-  },
-  type Category {
-    id: ID,
-    name: String,
-    decks: String,
-    user: String,
-  },
-  type Decks {
-    id: ID,
-    cards: String,
-    Name: String,
-    Toggle: Boolean,
-  },
-  type Cards {
-    id: ID,
-    front: String,
-    back: String,
-    correct: Number,
-    wrong: Number,
-    deck: String,
-    last: String,
-  }
-`;
- 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    get_cards: () => 'Hello world!',
-    get_one_card: () => 'Hello world!',
-    get_decks: () => 'Hello world!',
-    get_one_deck: () => 'Hello world!',
-  },
-  Mutation: {
-      edit_card: () => 'Hello Darkness',
-      edit_deck: () => 'My old friend',
-  }
-};
- 
 const server = new ApolloServer({ typeDefs, resolvers });
  
 const app = express();
