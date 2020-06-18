e707d1a318bcad8349616e819cca84d1e6efa57a
cmzhong <cheriemzhong@gmail.com>
2020-06-18 12:05:56 -0700

    Adding delete functionality to Cards_frame on trash icon

--- components/Cards_frame/Cards_frame.tsx
+++ components/Cards_frame/Cards_frame.tsx
@@ -3,73 +3,78 @@ import { View, ScrollView } from 'react-native'
 import { Layout, Text, Button, Icon, List, ListItem } from '@ui-kitten/components';
 
 
-const TrashIcon = (props: any) => (
-  <Icon name='trash-2-outline' {...props} />
-);
+export default function Cards_frame({ navigation }: any): React.ReactElement {
+  const [flashcards, setFlashcards] = useState([]);
 
-// ======================================
+  useEffect( () => {
+    const getCards = async() => {
+      try {
+        const data = await fetch("http://localhost:4000", {
+          method: "POST",
+          headers: { "Content-Type" : "application/json" },
+          body: JSON.stringify({
+            query: `
+              query {
+                cards {
+                  front
+                  back
+                }
+              }`
+          })
+        })
+        const flashcardsData = await data.json()
+        setFlashcards(flashcardsData.data.cards)
+      }
+      catch (err) {
+        console.log("Error in getCards: ", err)
+      }
+    }
+    getCards()}, [])
+  
+
+  let selected = {};
+
+  const setSelected = (item) => {
+    selected = item;
+  }
+
+  // ======================================
 // delete card API call to server to remove a card
 // ======================================
-const deleteCard: any = () => {
-  fetch("http://localhost:4000", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      query: `
-      mutation {
-        deleteCard(front: "${front}"){
-          front: front
+  const deleteCard: any = (selected) => {
+    console.log("selected: ", selected)
+    fetch("http://localhost:4000", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `
+        mutation {
+          deleteCard (front: "${selected.front}"){
+            front: front
+          }
         }
-      }
-     `
-    })
-  })
-    .then(res => res.json())
-    .then(data => console.log("deleted the cards. Check DB to see if it's actually gone"))
-}
-
-
-
-export default function Cards_frame({ navigation }: any): React.ReactElement {
-const [flashcards, setFlashcards] = useState([]);
-
-useEffect( () => {
-  const getCards = async() => {
-    try{
-      const data = await fetch("http://localhost:4000", {
-        method: "POST",
-        headers: { "Content-Type" : "application/json" },
-        body: JSON.stringify({
-          query: `
-            query {
-              cards {
-                front
-                back
-              }
-            }`
-        })
+      `
       })
-      const flashcardsData = await data.json()
-      setFlashcards(flashcardsData.data.cards)
-    }
-    catch (err) {
-      console.log("Error in getCards: ", err)
-    }
+    })
+      .then(res => res.json())
+      .then(data => console.log("Deleted Card:", data))
   }
-  getCards()}, [])
-  
-  const renderItemIcon = (props) => (
+
+
+  const renderItemIcon = (props: any) => (
     <View style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'flex-end'}}>
-      <Icon name='edit-outline' {...props} onClick={() => console.log("what up")} />
-      <Icon name='trash-2-outline' {...props} onClick={() => console.log("what up")} />
+      <Icon name='edit-outline' {...props} onPress={() => console.log("selected: ", selected)} />
+      <Icon name='trash-2-outline' {...props} onPress={() => deleteCard(selected)} />
     </View>
   );
   
+
   const renderItem = ({ item }) => (
     <ListItem
       title={`${item.front}`}
       description={`${item.back}`}
       accessoryRight={renderItemIcon}
+      onMouseEnter={() => setSelected(item)}
     />
   );
 
@@ -77,25 +82,29 @@ useEffect( () => {
     <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
 
       <Text category='h1' style={{padding:'1em'}}>Your Cards:</Text>
-      {/* <View style={{ flexDirection: 'row' }}> */}
-        {/* <Button
-          onPress={() => deleteCard()} // upon clicking the button, trigger fn to delete card
-          style={{ margin: '1em' }}
-          >Delete a Card</Button>
-        </ View> */}
+
       <View style={{maxHeight:'32em', padding:'1em'}}>
         <List
         data={flashcards}
         renderItem={renderItem}
         />
       </View>
-        <View style={{ flexDirection: 'row' }}>
-          <Button 
-            style={{ margin: '5%', padding:'1em' }} 
+
+      <View style={{ flexDirection: 'row' }}>
+        <Button 
+          style={{ margin: '5%', padding:'4em', shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4 }} 
             onPress={() => navigation.navigate('AddCard')}>
               Add a Card
-          </Button>
-        </ View>
+        </Button>
+      </ View>
+
     </Layout>
   )
 }
